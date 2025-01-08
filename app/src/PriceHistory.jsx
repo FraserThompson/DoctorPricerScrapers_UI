0d49b9a1bb6be8d6af86f3efc6dfae8ec53df44e
Fraser Thompson <fraserthompson0@gmail.com>
2025-01-09 12:36:44 +1300

    Remove average graphs from 2025 for now

--- app/src/PriceHistory.jsx
+++ app/src/PriceHistory.jsx
@@ -40,7 +40,7 @@ export default function PriceHistory({ data }) {
       for (var i = 0; i < ages.length; i++) {
         const age = ages[i];
 
-        if (!value[age] || label == "2019") {
+        if (!value[age] || label == "2019"  || label == "2025") {
           console.log(
             "Price History: Skipping " +
               label +
